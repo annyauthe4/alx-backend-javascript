b7bfb5a0697b5bcf18961c083cb78b61d16ac830
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-04-18 11:42:22 +0100

    Check argument type

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,7 +3,13 @@ import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = amount;
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._currency = currency;
   }
 
