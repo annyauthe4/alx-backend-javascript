916598552de52d35aae1631060f7ae7c564284f1
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-04-26 04:50:19 +0100

    Create string literal type

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,3 +62,15 @@ function executeWork(employee: DirectorInterface | TeacherInterface): void {
     console.log(employee.workTeacherTasks());
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return '';
+  }
+}
