a3d60946dc2864890c80eb51b3122cf2308b8aa9
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-11 03:45:42 +0100

    Use process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing\n');
+});
