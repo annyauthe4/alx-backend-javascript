c0e806d92113cec6ad3ab13338ab836b08c18889
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-11 05:06:06 +0100

    Create express server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// 6-http_express.js
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello ALX!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
