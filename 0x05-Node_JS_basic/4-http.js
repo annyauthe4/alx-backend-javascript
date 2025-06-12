982f831a05680f52342cd87a50aad38f5c9e059b
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-12 07:49:57 +0100

    Create server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
+const PORT = 1245;
+const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-  const responseMsg = 'Hello ALX!';
-  res.statusCode = 200;
+  const responseMsg = 'Hello Holberton School!';
+
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseMsg.length);
+  res.statusCode = 200;
   res.write(Buffer.from(responseMsg));
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
