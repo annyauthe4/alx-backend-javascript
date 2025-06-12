7ae1a3316c033443636bcb6965a7d055ee908d7f
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-12 11:36:17 +0100

    Create routes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,30 +2,59 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 // Get database file path from command-line argument
-const database = process.argv[2];
+const db_file = process.argv[2];
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+const SERVER_ROUTE_HANDLERS = [
+  {
+    route: '/',
+    handler(_, res) {
+      const responseTxt = 'Hello Holberton School!';
 
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Hello ALX!');
-  } else if (req.url === '/students') {
-    countStudents(database)
-      .then((data) => {
-        res.statusCode = 200;
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((err) => {
-        res.statusCode = 500;
-        res.end(`This is the list of our students\n${err.message}`);
-      });
-  } else {
-    res.statusCode = 200;
-    res.end('Hello ALX!');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseTxt.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseTxt));
+    },
+  },
+  {
+    route: '/students',
+    handler(_, res) {
+      const resParts = ['This is the list of our students'];
+
+      countStudents(db_file)
+        .then((report) => {
+          resParts.push(report);
+          const responseTxt = resParts.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseTxt.length);
+          res.statusCode = 200;
+          res.end(responseTxt);
+        })
+        .catch((err) => {
+          resParts.push(err instanceof Error ? err.message : err.toString());
+          const responseTxt = resParts.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseTxt.length);
+          res.statusCode = 200;
+          res.end(responseTxt);
+        });
+    },
+  },
+];
+
+app.on('request', (req, res) => {
+  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
+    if (routeHandler.route === req.url) {
+      routeHandler.handler(req, res);
+      break;
+    }
   }
 });
-
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
