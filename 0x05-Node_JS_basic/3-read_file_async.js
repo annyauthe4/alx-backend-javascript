1ef7871b64e612eae29d310ba12eb6176dfa0b5e
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-12 10:22:45 +0100

    Correct async readfile

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,11 +9,46 @@ function countStudents(path) {
       }
 
       const lines = data
+        .toString('utf-8')
         .split('\n')
         .map((line) => line.trim())
         .filter((line) => line.length > 0);
 
-      if (lines.length <= 1) {
+      const studentGroups = {};
+      const dbFieldNames = lines[0].split(',');
+
+      // Get students' Property keys e.g age, name
+      const studentPptyKeys = dbFieldNames.slice(0, -1);
+
+      // Get the value for each key
+      for (const line of lines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPptyValues = studentRecord.slice(0, -1);
+        const field = studentRecord[studentRecord.length - 1];
+
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
+        }
+        const studentEntries = studentPptyKeys
+          .map((Key, idx) => [Key, studentPptyValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
+      }
+
+      const totalStudents = Object
+        .values(studentGroups)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const names = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${names}`);
+      }
+
+      resolve(true);
+    });
+  });
+}
+/* if (lines.length <= 1) {
         console.log('Number of students: 0');
         resolve();
         return;
@@ -47,6 +82,6 @@ function countStudents(path) {
       resolve();
     });
   });
-}
+} */
 
 module.exports = countStudents;
